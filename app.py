2a7b1444b45647fc1085c97e358731ab0c338895
KSheeds1 <katiesheedy@hotmail.com>
2021-10-13 10:20:07 +0000

    Build out the POST method to complete registration functionality, using Werkzeug's standard password hashing and RegEx expression patterns to program defensively protecting both site and user. Add somgeneral styling to the app and reg form.

--- app.py
+++ app.py
@@ -17,16 +17,38 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
-
+"""
+App route for the landing page of Gym Rat Dictionaries. 
+"""
 @app.route("/")
 @app.route("/get_definitions")
 def get_definitions():
     definitions = mongo.db.definitions.find()
     return render_template("definitions.html", definitions=definitions)
 
-
+"""
+App route for the registration functionality and page.
+"""
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # Check if username already exists in the db:
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        if existing_user:
+            flash("Sorry! This username already exists")
+            return redirect(url_for("register"))
+
+        # 'register' is the dict that will be inserted in to the db:
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.insert_one(register)
+
+        # Put the new user into 'session' cookie:
+        session["user"] = request.form.get("username").lower()
+        flash("Registration successful!")
     return render_template("register.html")
 
 
