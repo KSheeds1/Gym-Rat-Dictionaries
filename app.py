8db6fd50a19d3c1899700f999f45a19e6400ae0a
KSheeds1 <katiesheedy@hotmail.com>
2021-10-13 15:06:40 +0000

    Build the logout functionality to remove a user from session cookies and specify the session cookie to delete. Add a further layer of defensive programming to the profile function. Use template logic to update nav links depending on user status: logged in/out user or admin.

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def login():
                 flash("Incorrect Username or Password")
                 return redirect(url_for("login"))
 
-        else: 
+        else:
             # Username doesn't exist:
             flash("Incorrect Username or Password")
             return redirect(url_for("login"))
@@ -99,13 +99,33 @@ def login():
 def profile(username):
     """
     Once logged in or registered, the profile function will be triggered
-    and will redirect the session user to their profile page.
+    and will redirect the session user to their profile page. 
     """
     # Grab the session user's username from the db:
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    # If session user cookie truthy, return to appropriate profile:
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    # If false or doesn't exist, redirect to login:
+    return redirect(url_for("login"))
     
+
+@app.route("/logout")
+def logout():
+    """
+    Removes user from session cookie and specifies which
+    session cookie to delete. Before logging the user out,
+    they will get a flash message advising that they have
+    been logged out and will be redirected to login. 
+    """
+    # Remove user from session cookie:
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
