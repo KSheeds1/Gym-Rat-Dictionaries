0350aa10b4c26ea92b9a8567637dfe3dc1a151ff
KSheeds1 <katiesheedy@hotmail.com>
2021-11-06 17:24:34 +0000

    Set debug to False for submission.

--- app.py
+++ app.py
@@ -666,4 +666,4 @@ def generic(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
